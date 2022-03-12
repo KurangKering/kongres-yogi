5f7e7c0870b57cfa6962544af21a99e7c04234fa
KurangKering <cybercature@gmail.com>
2022-03-13 01:55:43 +0700

    tes send mail

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -116,7 +116,7 @@ class Email extends BaseConfig
      *
      * @var integer
      */
-    public $priority = 3;
+    public $priority = 1;
     /**
      * Newline character. (Use “\r\n” to comply with RFC 822)
      *
