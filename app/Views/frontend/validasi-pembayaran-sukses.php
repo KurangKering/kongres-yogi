df85931be8966ec44d5ad6b482b7584d8d7a7c2c
KurangKering <cybercature@gmail.com>
2022-03-13 03:54:13 +0700

    email validasi

--- app/Views/frontend/validasi-pembayaran-sukses.php
+++ app/Views/frontend/validasi-pembayaran-sukses.php
@@ -3,15 +3,25 @@
 <div class="page-header header-filter" filter-color="purple">
     <div class="container">
         <div class="row">
-            <div class="col-md-10 col-md-offset-1">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <div class="icon icon-success">
+                                <i class="material-icons" style="font-size: 4.4em">verified_user</i>
+                            </div>
+                            <h4 class="info-title">Berhasil submit bukti pembayaran</h4>
+                            <?php $email = $data['pendaftaran']['email']; ?>
+                            <p>Bukti pembayaran telah berhasil disubmit. Hasil verifikasi bukti pembayaran akan diumumkan melalui email. Silahkan cek email secara berkala. </p>
 
-                <div class="card card-signup">
-                    sukses validasi
+                        </div>
+                    </div>
                 </div>
 
             </div>
         </div>
     </div>
+</div>
 
 </div>
 <?= $this->endSection() ?>
