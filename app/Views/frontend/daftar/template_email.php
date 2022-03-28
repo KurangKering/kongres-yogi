a25acfbbcc0c3678f5cc2b8b8628df7286164a5b
yorugime <37960135+yorugime@users.noreply.github.com>
2022-03-28 16:17:29 +0700

    Update template_email.php

--- app/Views/frontend/daftar/template_email.php
+++ app/Views/frontend/daftar/template_email.php
@@ -2,67 +2,10 @@
 
 <head>
 
-    <style type="text/css">
+    <style>
         #table-identitas th {
             text-align: left;
         }
-		
-				body {
-			font-family: "Lucida Sans Unicode", "Lucida Grande", "Segoe Ui";
-		}
-		
-		/* Table */
-		.bayar-table {
-			border-collapse: collapse;
-			font-size: 13px;
-		}
-		.bayar-table th, 
-		.bayar-table td {
-			border: 1px solid #e1edff;
-			padding: 7px 17px;
-		}
-		.bayar-table .title {
-			caption-side: bottom;
-			margin-top: 12px;
-		}
-		
-		/* Table Header */
-		.bayar-table thead th {
-			background-color: #508abb;
-			color: #FFFFFF;
-			border-color: #6ea1cc !important;
-			text-transform: uppercase;
-		}
-		
-		/* Table Body */
-		.bayar-table tbody td {
-			color: #353535;
-		}
-		.bayar-table tbody td:first-child,
-		.bayar-table tbody td:last-child,
-		.bayar-table tbody td:nth-child(4) {
-			text-align: right;
-		}
-		.bayar-table tbody tr:nth-child(odd) td {
-			background-color: #f4fbff;
-		}
-		.bayar-table tbody tr:hover td {
-			background-color: #ffffa2;
-			border-color: #ffff0f;
-			transition: all .2s;
-		}
-		
-		/* Table Footer */
-		.bayar-table tfoot th {
-			background-color: #e5f5ff;
-		}
-		.bayar-table tfoot th:first-child {
-			text-align: left;
-		}
-		.bayar-table tbody td:empty
-		{
-			background-color: #ffcccc;
-		}
     </style>
 </head>
 
@@ -71,7 +14,7 @@
     <div class="container">
 
         <h2>
-            Pendaftaran KOGI XVIII 2022 PEKANBARU Berhasil.
+            Pendaftaran KOGI XVIII PEKANBARU 2022 Berhasil.
         </h2>
 
         <?php
@@ -81,12 +24,17 @@
         $batas =  date('Y-m-d H:i:s', $newdate);
         ?>
 
-       
+        <ol>
+            Lakukan langkah-langkah berikut ini:
+            <li>Lakukan pembayaran sebesar <b><?= rupiah($pendaftaran['biaya'] + $pendaftaran['kode_unik_pembayaran']) ?> </b> sebelum tanggal <b><?= indoDate($batas, 'd-m-Y') ?> Pukul <?= indoDate($batas, 'H:i:s') ?></b></li>
+            <li>Simpan bukti pembayaran dalam bentuk gambar berformat JPG/PNG</li>
+            <li>Upload bukti pembayaran melalui link <?= base_url('validasi-pembayaran') ?></li>
+        </ol>
 
         <h2>Data Identitas Pendaftar</h2>
         <table id="table-identitas">
             <tr>
-                <th>No. PENDAFTARAN</th>
+                <th>No Pendaftaran</th>
                 <td>:</td>
                 <td><?= $pendaftaran['id_pendaftaran'] ?></td>
             </tr>
@@ -117,62 +65,65 @@
             </tr>
         </table>
 
-       
+        <h2>Data Simposium</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Simposium</th>
+                    <th>Date</th>
+                    <th>Tipe Pendaftaran</th>
+                    <th width="150px">Biaya</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><?= $pendaftaran['kategori'] ?></td>
+                    <td><?= $pendaftaran['hybrid'] ?></td>
+                    <td><?= tipePendaftaran($pendaftaran['tipe_pendaftaran']) ?></td>
+                    <td><?= rupiah($pendaftaran['harga']) ?></td>
+                </tr>
+            </tbody>
+        </table>
+
         <?php if (!empty($workshops)) : ?>
-            
-            <table  class="bayar-table">
+            <h2>Data Workshop</h2>
+
+            <table class="table">
                 <thead>
                     <tr>
                         <th>No</th>
-                        <th>Deskripsi</th>
+                        <th>Pelatihan</th>
+                        <th>Waktu</th>
                         <th>Tempat</th>
                         <th width="150px">Biaya</th>
                     </tr>
                 </thead>
-				
                 <tbody>
-				<tr>
-					<td>1</td>
-                    <td><?= $pendaftaran['kategori'] ?></td>
-                    <td><?= $pendaftaran['hybrid'] ?></td>
-                    <td><?= rupiah($pendaftaran['harga']) ?></td>
-                </tr>
                     <?php $no = 1; ?>
                     <?php foreach ($workshops as $k => $v) : ?>
                         <tr>
-                            <td><?= $no+=1; ?></td>
+                            <td><?= $no++; ?></td>
                             <td><?= $v['pelatihan'] ?></td>
+                            <td><?= indoDate($v['waktu'], 'd-m-Y') . ' Pukul ' . indoDate($v['waktu'], 'H:i') ?></td>
                             <td><?= $v['tempat'] ?></td>
                             <td><?= rupiah($v['biaya']) ?></td>
                         </tr>
                     <?php endforeach ?>
-				<tr>
-                    <th colspan="2">Kode Unik Pembayaran</th>
-                    <th colspan="2"><?= rupiah($pendaftaran['kode_unik_pembayaran']) ?></th>
-				</tr>
-				<tr>
-                    <th colspan="2">TOTAL PEMBAYARAN SEBESAR</th>
-                    <th colspan="2"><?= rupiah($pendaftaran['biaya'] + $pendaftaran['kode_unik_pembayaran']) ?></th>
-				</tr>
                 </tbody>
             </table>
         <?php endif ?>
-        
-		</br>
-		
-		 <ol>
-            <b>INFORMASI PEMBAYARAN:</b>
-            <li>Lakukan pembayaran melalui transfer Bank</b>
-                <b>No. Rekekening : 1720-01-002204-53-2</b>
-                <b>Nama Rekening: PANITIA KOGI 18 PEKANBARU</b></li>
-            <li>Screenshot bukti Transfer anda yang telah berhasil <b>atau</b> Foto struk Bukti Transfer anda dengan jelas lalu Screenshot hasil foto tersebut (bertujuan agar ukuran file kurang dari 1MB)</li>
-            <li>Upload bukti pembayaran dengan No. Pendaftaran berikut melalui link :  
-			<?= base_url('validasi-pembayaran') ?></li>
-			
-        </ol>
-		</br>
-		<b>Informasi lebih lanjut. WA: 081374391461(Lira)</b>
-		
+        <table class="table">
+            <thead>
+                <tr>
+                    <th class="text-right">Kode Unik Pembayaran</th>
+                    <th width="150px"><?= rupiah($pendaftaran['kode_unik_pembayaran']) ?></th>
+                </tr>
+                <tr>
+                    <th class="text-right">Total</th>
+                    <th width="150px"><?= rupiah($pendaftaran['biaya'] + $pendaftaran['kode_unik_pembayaran']) ?></th>
+                </tr>
+            </thead>
+        </table>
     </div>
 
 </body>
