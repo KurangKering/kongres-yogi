60cdd7014538ef0f6a335148312e2fca5df86171
yorugime <37960135+yorugime@users.noreply.github.com>
2022-04-01 09:49:48 +0700

    Update sidebar.php

--- app/Views/backend/template/sidebar.php
+++ app/Views/backend/template/sidebar.php
@@ -27,7 +27,7 @@
                 
                 <li class="nav-item">
                     <a href="<?= base_url('backend/pendaftaran') ?>" class="nav-link">
-                        <i class="glyphicon glyphicon-thumbs-up"></i>
+                        <i class="nav-icon glyphicon glyphicon-thumbs-up"></i>
                         <p>
                             Data Pendaftaran
                         </p>
