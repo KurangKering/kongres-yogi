f92efad2ca79c4bde1324c1e3b590dbe533db3b0
yorugime <37960135+yorugime@users.noreply.github.com>
2022-06-23 15:38:16 +0700

    Update index.php

--- app/Views/frontend/menu-lain/index.php
+++ app/Views/frontend/menu-lain/index.php
@@ -1,6 +1,6 @@
 <?= $this->extend('frontend/template/layout') ?>
 <?= $this->section('content') ?>
-<div class="page-header header-filter" background="BG1.png" filter-color="purple">
+<div class="page-header header-filter" filter-color="purple">
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
